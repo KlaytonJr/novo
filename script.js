ad124509a77b35cc9caafa4a463ea8c1ae082e7c
Klayton Junior <klaytonjunior@protonmail.com>
2024-10-09 21:18:38 -0300

    Adicionando o script da aplicação

---
+++ script.js
@@ -0,0 +1,69 @@
+console.log("Dentro do arquivo")
+
+
+
+
+
+console.log("depois de espaço")
+
+// comentário de única linha
+
+/*
+ comentário de múltiplas linhas
+
+
+*/
+
+// var nome_aluno = "Klayton"
+// console.log('👉 nome_aluno => ', nome_aluno);
+
+// var num_a = 3;
+// var num_b = 7;
+// var num_c = 10;
+
+// var somaNumAComNumB = num_a + num_b;
+
+// console.log("somaNumAComNumB ", somaNumAComNumB)
+// console.log("num_a - num_b => ", num_a - num_b, "outro texto")
+// console.log("num_a * num_b => ", num_a * num_b)
+// console.log("num_a / num_b => ", num_a / num_b)
+// console.log("num_a ** num_b => ", num_a ** num_b)
+// console.log("num_c % num_a => ", num_c % num_a)
+
+// console.log((3 + 5) * 2)
+
+// var nome = "Klayton";
+// var sobrenome = "Marcos";
+
+// console.log(nome + " " + sobrenome);
+// console.log(nome + num_b);
+
+// var num_string = "2";
+
+// console.log("num_string + num_b", num_string + num_b)
+
+// var nomeInserido = prompt("Digite seu nome")
+
+// console.log(nomeInserido);
+// // console.error("Deu erro")
+// // console.warn("Deu warn")
+
+// alert("Bem vindo, " + nomeInserido)
+
+// var entrada = prompt("Digite uma letra")
+
+// // var saida = entrada + " " + "entrada"
+// var saida = entrada + " entrada"
+
+// alert(saida);
+
+var numero1 = parseInt(prompt("Digite um numero"))
+var numero2 = parseInt(prompt("Digite um numero"))
+
+// var numeroInt1 = parseInt(numero1);
+// var numeroInt2 = parseInt(numero2);
+// var numeroFloat = parseFloat(numero);
+
+var saida = numero1 + numero2
+
+alert(saida);
